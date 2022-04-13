088863c6eb635b99efdd32bb2bad3d798a973035
Tuyen Le <tuyenlnse@gmail.com>
2022-04-13 09:44:06 +0700

    init project nextjs tailwindcss web3 blockchain solidity ecommerce

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: "media", 
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx, html}",
+    "./components/**/*.{js,ts,jsx,tsx, html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
