3f4a62ff7b9ee034fe918b7fde27123a1ba3b075
TuyenLN <tuyenlnse@gmail.com>
2022-06-07 18:05:50 +0700

    fake data get list

--- components/ui/common/CourseList/index.js
+++ components/ui/common/CourseList/index.js
@@ -4,12 +4,12 @@ const CourseList = ({ post }) => {
     return (
         <section className={course.wrapper}>
             <div className={course.image}>
-                <Image src="/images/courselist/image1.png" width={1920} height={1080} />
+                <Image src={post.image} width={1920} height={1080} />
             </div>
             <div className={course.info}>
                 <div>
-                    <h1 className={course.title}>Title</h1>
-                    <p className={course.desc}>Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur</p>
+                    <h1 className={course.title}>{post.title}</h1>
+                    <p className={course.desc}>{post.desc}</p>
                 </div>
                 <div className={course.button}>
                     <button>Purchase</button>
--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,56 @@ import { CourseList } from "@components/ui/common";
 export default function Home() {
   return (
       <section>
-        <CourseList posts={post} />
+        <div className="grid grid-cols-2 gap-4 max-w-[1440px] mx-auto">
+        {posts.map((post) => (
+          <CourseList post={post} />
+        ))}
+        </div>
       </section>
   )
 }
 
 Home.Layout = BaseLayout;
+
+const posts = [
+  {
+    _id: "121v33hj4b34",
+    title: "solidity",
+    desc: "learn solidity",
+    image: "/images/courselist/solidity.png",
+    author: "tuyenln",
+    slug: "Solidity"
+  },
+  {
+    _id: "hn3434h34903943j43j",
+    title: "Learning Rust",
+    desc: "Rust",
+    image: "/images/courselist/rust.png",
+    author: "TuyenLN",
+    slug: "Rust"
+  },
+  {
+    _id: "hn34fefe03943j43j",
+    title: "Web3",
+    desc: "Web3 Mastering",
+    image: "/images/courselist/web3.jpg",
+    author: "TuyenLN",
+    slug: "Web3"
+  },
+  {
+    _id: "hn34fefe0394edfefj",
+    title: "Next JS",
+    desc: "Next Js",
+    image: "/images/courselist/next.png",
+    author: "TuyenLN",
+    slug: "nextjs"
+  },
+  {
+    _id: "hn34gegeg9egeg94edfefj",
+    title: "BlockChain",
+    desc: "BlockChain",
+    image: "/images/courselist/blockchain.jpg",
+    author: "TuyenLN",
+    slug: "blockchain"
+  }
+]
